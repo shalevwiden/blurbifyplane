f3d0bd6107a2903b3161c43800f4aa53c21412a0
Shalev Widen <shalevwiden@gmail.com>
2025-11-05 16:58:39 -0600

    Serious plane design progress

--- static/js/animations.js
+++ static/js/animations.js
@@ -1,19 +1,18 @@
-const editedimage = document.querySelector("#editedimage");
+const plane = document.querySelector(".plane");
 
 const buttonsdiv = document.querySelector(".buttonsdiv");
 
-const gradient1button = buttonsdiv.querySelector("#gradient1button");
+const shifthuebutton = buttonsdiv.querySelector("#shifthuebutton");
 const gradient2button = buttonsdiv.querySelector("#gradient2button");
 const gradient3button = buttonsdiv.querySelector("#gradient3button");
 
 const applyGradient = (newClass) => {
-  editedimage.className = ""; // clear all previous gradients
-  editedimage.classList.add(newClass); // apply the new one
+  plane.classList.add(newClass); // apply the new one
 };
 
-gradient1button.addEventListener("click", () => {
-  console.log("trying gradient 1");
-  applyGradient("gradient1class");
+shifthuebutton.addEventListener("click", () => {
+  console.log("applying hue shit");
+  plane.classList.toggle("hueshiftclass");
 });
 gradient2button.addEventListener("click", () => {
   console.log("trying gradient 2");
